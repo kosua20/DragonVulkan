011266f4d6ccdc0b9aaa66335cd771d02d38869e
sirodrig <simon.rodriguez@inria.fr>
2018-07-06 14:08:44 +0200

    Setup GLFW and Vulkan instance.

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,68 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-
 #include <iostream>
 
+
+
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
 int main() {
-	glfwInit();
 
+	/// Init GLFW3.
+	if(!glfwInit()){
+		std::cerr << "Unable to initialize GLFW." << std::endl;
+		return 1;
+	}
+	// Don't create an OpenGL context.
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Dragon Vulkan", nullptr, nullptr);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	// Create window.
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Dragon Vulkan", nullptr, nullptr);
+	if(!window){
+		std::cerr << "Unable to create GLFW window." << std::endl;
+		return 2;
+	}
+	/// Init Vulkan.
+	// Create a Vulkan instance.
+	VkInstance instance;
+	// Creation setup.
+	VkApplicationInfo appInfo = {};
+	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	appInfo.pApplicationName = "Dragon Vulkan";
+	appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.pEngineName = "No engine";
+	appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.apiVersion = VK_API_VERSION_1_0;
+	VkInstanceCreateInfo createInfo = {};
+	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	createInfo.pApplicationInfo = &appInfo;
+	// Default Vulkan has no notion of surface/window. GLFW provide an implementation of the corresponding KHR extension.
+	// We have to tell Vulkan it exists.
+	uint32_t glfwExtensionCount = 0;
+	const char** glfwExtensions;
+	// Get GLFW exposed extensions.
+	glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+	// Add them to the instance infos.
+	createInfo.enabledExtensionCount = glfwExtensionCount;
+	createInfo.ppEnabledExtensionNames = glfwExtensions;
+	// Validation layers.
+	createInfo.enabledLayerCount = 0;
 
-	uint32_t extensionCount = 0;
-	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+	if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+		std::cerr << "Unable to create a Vulkan instance." << std::endl;
+		return 3;
+	}
 
-	std::cout << extensionCount << " extensions supported" << std::endl;
-
-	glm::mat4 matrix;
-	glm::vec4 vec;
-	auto test = matrix * vec;
-
+	/// Main loop.
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 	}
 
+	/// Cleanup.
+	vkDestroyInstance(instance, nullptr);
 	glfwDestroyWindow(window);
-
 	glfwTerminate();
 
 	return 0;
